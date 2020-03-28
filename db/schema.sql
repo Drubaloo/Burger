035c852e462a3f87a1822ff7bcdae394e78c569e
Drubaloo <Drubaloo@outlook.com>
2020-03-28 10:44:32 -0700

    added JAWSDB

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
